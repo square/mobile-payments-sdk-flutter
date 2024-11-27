766bad0f1bfabe520d52b7eb38d8d465ec5a5430
Plinio Correa <plinio@squareup.com>
2024-11-27 14:13:24 -0800

    Initial commit

---
+++ lib/square_mobile_payments_sdk_method_channel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'square_mobile_payments_sdk_platform_interface.dart';
+
+/// An implementation of [SquareMobilePaymentsSdkPlatform] that uses method channels.
+class MethodChannelSquareMobilePaymentsSdk extends SquareMobilePaymentsSdkPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('square_mobile_payments_sdk');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+}
