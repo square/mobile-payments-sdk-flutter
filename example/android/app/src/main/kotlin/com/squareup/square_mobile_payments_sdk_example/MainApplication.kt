362be75d03ba0262f9884f0c404507097ffb8fd0
Nilay-squareup <nilay@squareup.com>
2024-12-30 13:19:26 +0530

    PR comment resolved

--- example/android/app/src/main/kotlin/com/squareup/square_mobile_payments_sdk_example/MainApplication.kt
+++ example/android/app/src/main/kotlin/com/squareup/square_mobile_payments_sdk_example/MainApplication.kt
@@ -1,13 +1,11 @@
 package com.squareup.square_mobile_payments_sdk_example
 
 import android.app.Application
-import com.facebook.soloader.SoLoader
 import com.squareup.sdk.mobilepayments.MobilePaymentsSdk
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
         MobilePaymentsSdk.initialize("your-square-application-id", this)
     }
 }
