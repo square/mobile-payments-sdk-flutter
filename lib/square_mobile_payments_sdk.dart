bf724eda15334a0c1de255acbf78467e41327158
Cesar Sosa <101717495+cesar-sosa-hol@users.noreply.github.com>
2025-04-10 18:02:02 +0200

    export models and remove direct model imports

--- lib/square_mobile_payments_sdk.dart
+++ lib/square_mobile_payments_sdk.dart
@@ -4,6 +4,8 @@ import 'package:square_mobile_payments_sdk/src/managers/reader_manager.dart';
 import 'package:square_mobile_payments_sdk/src/managers/settings_manager.dart';
 import 'package:square_mobile_payments_sdk/src/managers/tap_to_pay_settings.dart';
 
+export 'package:square_mobile_payments_sdk/src/models/models.dart';
+
 class SquareMobilePaymentsSdk {
   final TapToPaySettings tapToPaySettings = TapToPaySettings();
   final AuthManager authManager = AuthManager();
